bfabe8a910ac960d6b0b2ac237180db3be7af4b9
igormeskoski <imeskoski@reetek.com.au>
2017-09-06 15:49:19 +1000

    logo fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ import { AppState } from './app.service';
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
-  public logo = 'assets/img/logo-blue.png';
+  public logo = 'assets/img/logo-blue-small.png';
   public angularclassLogo = 'assets/img/angularclass-avatar.png';
   public name = 'Angular 2 Webpack Starter';
   public url = 'https://twitter.com/AngularClass';
